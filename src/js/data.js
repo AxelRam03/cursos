bdb7a0f9ab47980ed6fae5a5df7ea70625a288f1
Axel Ramirez <axelr2336@gmail.com>
2026-03-24 20:09:09 -0600

    Fix: data.js limpio y sin errores JS. Plataforma y login desbloqueados.

--- src/js/data.js
+++ src/js/data.js
@@ -30,3 +30,8 @@ window.LESSONS = {
     content: '<h2>Variables en JavaScript</h2><p>Aprende la diferencia entre <b>var</b>, <b>let</b> y <b>const</b>.</p>'
   }
 };
+
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth-compat.js"></script>
+<script src="src/js/data.js"></script>
+<script src="src/js/app.js"></script>
